275833421a369a290a58b019dc6924f6ddb6e14f
kongta <1255617118@qq.com>
2019-04-22 23:56:01 +0800

    商品详情页增加购物车数量

--- lib/pages/detail_page/detail_bottom.dart
+++ lib/pages/detail_page/detail_bottom.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provide/provide.dart';
 import '../../provide/goods_detail.dart';
 import '../../provide/cart_provide.dart';
 import '../../provide/currentIndex.dart';
+import 'package:provide/provide.dart';
 class DetailBottom extends StatelessWidget {
 
   @override
@@ -14,22 +16,40 @@ class DetailBottom extends StatelessWidget {
       height: ScreenUtil().setHeight(90),
       child: Row(
         children: <Widget>[
-          InkWell(
-            onTap: (){
-              Provide.value<CurrentIndexProvide>(context).changeIndex(2);
-              Navigator.pop(context);
-            },
-            child:  Container(
-            height: ScreenUtil().setHeight(90),
-            width: ScreenUtil().setWidth(150),
-            color: Colors.white,
-            alignment: Alignment.center,
-            child: Icon(
-              Icons.add_shopping_cart,
-              color: Colors.pink,
-            )
-            ),
-          ),
+          Provide<CartProvide>(builder: (context,child,val){
+            return Stack(
+              children: <Widget>[
+                  InkWell(
+                    onTap: (){
+                      Provide.value<CurrentIndexProvide>(context).changeIndex(2);
+                      Navigator.pop(context);
+                    },
+                      child:  Container(
+                      height: ScreenUtil().setHeight(90),
+                      width: ScreenUtil().setWidth(150),
+                      color: Colors.white,
+                      alignment: Alignment.center,
+                      child: Icon(
+                        Icons.add_shopping_cart,
+                        color: Colors.pink,
+                      )
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 3,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.pink,
+                        borderRadius: BorderRadius.circular(12.0)
+                      ),
+                      padding: EdgeInsets.fromLTRB(6, 3, 6, 3),
+                      child: Text('${val.allCount}',style: TextStyle(color: Colors.white,fontSize:ScreenUtil().setSp(20)),),
+                    ),
+                  )
+              ],
+            );
+          }),
           InkWell(
             onTap: () async{
               await Provide.value<CartProvide>(context).addCart(goodsInfo.goodsId, goodsInfo.goodsName, 1, goodsInfo.presentPrice, goodsInfo.image1);
